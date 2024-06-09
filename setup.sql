9f45eadf9bd1720ae17a8d2d7d5fea7ceaf86c98
Aycan <aycan.biilge@gmail.com>
2024-06-09 19:52:53 -0400

    added dealerships table

--- setup.sql
+++ setup.sql
@@ -3,3 +3,11 @@ DROP DATABASE IF EXISTS dealership_db;
 CREATE DATABASE IF NOT EXISTS dealership_db;
 USE dealership_db;
 
+CREATE TABLE Dealerships (
+	Dealership_id int NOT NULL auto_INCREMENT,
+	Name varchar(50) NOT NULL,
+	Address varchar(50),
+	Phone varchar(12),
+    PRIMARY KEY (dealership_id)
+ );
+
